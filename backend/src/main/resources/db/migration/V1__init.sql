bb49c78cd87edc1937ec52c5517bf76a05c09fcb
Gard Alhaug <gardalhaug@gmail.com>
2025-04-23 13:39:06 +0200

    Added docker-compose.yml for postgres and migrations

---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+
+-- Bare å endre på dette asså, er mest for testing
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
