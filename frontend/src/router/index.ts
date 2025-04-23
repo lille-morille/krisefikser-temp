0b356c2cb5ffea4cd6bd7f586cfd0fd8e8b42ab4
Mori <anders.morille@gmail.com>
2025-04-23 13:25:02 +0200

    Added logo and signin page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/signin',
+      name: 'sign-in',
+      component: () => import('../views/auth/SignInView.vue'),
+    },
   ],
 })
 
