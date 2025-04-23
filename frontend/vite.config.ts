9d47f8308534877e8f51567ff8b48626ae72eed4
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-04-23 09:25:57 +0200

    ts confing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -10,7 +8,7 @@ export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': '/src',
     },
   },
 })
