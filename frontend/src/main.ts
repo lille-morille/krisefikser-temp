da29ea3d9937fd8859d9d16e03c57802931fe8c8
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-04-24 14:04:16 +0200

    added navigation guards

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+router.beforeEach((to, _, next) => {
+  // TODO: Check JWT validity
+  const isAuthenticated = false
+
+  if (['sign-in', 'sign-up', 'forgot-password'].includes(to.name as string)) {
+    // TODO: De-auth user?
+
+    next()
+  } else if (isAuthenticated) {
+    next()
+  } else {
+    next({ name: 'sign-in' })
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
